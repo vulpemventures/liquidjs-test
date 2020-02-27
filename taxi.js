2858b1629737cdfbc92e5d241a740a35fabec5cd
Marco Argentieri <marco@vulpem.com>
2020-02-27 12:07:46 +0100

    Separate building from signing

--- taxi.js
+++ taxi.js
@@ -26,29 +26,30 @@ function utxosByAsset(utxos, asset) {
   })
 }
 
+// generate a keyPair importing from WIF
+//Treasury WIF
+const keyPair = liquid.ECPair.fromWIF(
+  'cPNMJD4VyFnQjGbGs3kcydRzAbDCXrLAbvH6wTCqs88qg1SkZT3J',
+  network,
+);
+const keyPair2 = liquid.ECPair.fromWIF(
+  'cSv4PQtTpvYKHjfp9qih2RMeieBQAVADqc8JGXPvA7mkJ8yD5QC1',
+  network,
+);
 
-async function main() {
-  // generate a keyPair importing from WIF
-  const keyPair = liquid.ECPair.fromWIF(
-    'cPNMJD4VyFnQjGbGs3kcydRzAbDCXrLAbvH6wTCqs88qg1SkZT3J',
-    network,
-  );
-  const keyPair2 = liquid.ECPair.fromWIF(
-    'cSv4PQtTpvYKHjfp9qih2RMeieBQAVADqc8JGXPvA7mkJ8yD5QC1',
-    network,
-  );
+const keyPair3 = liquid.ECPair.fromWIF(
+  'cPMd8Erwxc8cW9dEf23LbzYGpiCZXrUWPWiHyibTfQwvRHKRHWX2',
+  network
+);
 
-  const keyPair3 = liquid.ECPair.fromWIF(
-    'cPMd8Erwxc8cW9dEf23LbzYGpiCZXrUWPWiHyibTfQwvRHKRHWX2',
-    network
-  );
+
+async function build() {
 
   // Get a random unconfidential liquid address to request funds
-  const charlie = liquid.payments.p2wpkh({ pubkey: keyPair.publicKey, network });
   const bob = liquid.payments.p2wpkh({ pubkey: keyPair2.publicKey, network });
   const darlene = liquid.payments.p2wpkh({ pubkey: keyPair3.publicKey, network });
   console.log(`Bob address ${bob.address}`);
-  console.log(`charlie address ${charlie.address}`);
+  console.log(`darlene address ${darlene.address}`);
   console.log(``);
 
   let bobUtxos = await fetchUtxos(bob.address, TIERO_ASSET_HASH);
@@ -56,7 +57,10 @@ async function main() {
   // Now we can try to spend the fresh utxo
   const psbt = new liquid.Psbt();
 
-  const bobUtxo = bobUtxos[0];
+  const amountForDarlene = 1000000
+
+
+  const bobUtxo = bobUtxos.find(u => u.value > amountForDarlene);
   psbt.addInput({
     hash: bobUtxo.txid,
     index: bobUtxo.vout,
@@ -68,32 +72,38 @@ async function main() {
     },
   })
 
+  const change = bobUtxo.value - amountForDarlene;
 
   psbt.addOutputs([
     // Darlene new output 
     {
       nonce: Buffer.from('00', 'hex'),
-      value: liquid.satoshiToConfidentialValue(5000000000),
+      value: liquid.satoshiToConfidentialValue(amountForDarlene),
       script: darlene.output,
       asset: TieroAsset,
     },
     // Change of BOB's assets
     {
       nonce: Buffer.from('00', 'hex'),
-      value: liquid.satoshiToConfidentialValue(5000000000),
+      value: liquid.satoshiToConfidentialValue(change),
       script: bob.output,
       asset: TieroAsset,
     },
 
   ]);
 
-  const base64 = psbt.toBase64();
+  return psbt.toBase64();
+}
 
+
+async function payFees() {
+  // This is the treasury
+  const charlie = liquid.payments.p2wpkh({ pubkey: keyPair.publicKey, network });
   await faucet(charlie.address);
   const charlieUtxos = await fetchUtxos(charlie.address, network.assetHash);
   const input = charlieUtxos[0];
-  
-  
+
+
   const decoded = liquid.Psbt.fromBase64(base64);
   decoded.addInput({
     hash: input.txid,
@@ -105,12 +115,12 @@ async function main() {
       asset: LiquidBitcoinAsset
     },
   })
-  
+
   const FEE = 800
-  const changeOutputValue = input.value - FEE; 
+  const changeOutputValue = input.value - FEE;
 
   decoded.addOutputs([
-    //Change of Caarlie 
+    //Change of Charlie 
     {
       nonce: Buffer.from('00', 'hex'),
       value: liquid.satoshiToConfidentialValue(changeOutputValue),
@@ -126,21 +136,15 @@ async function main() {
     }
   ])
 
+  return decoded.toBase64()
 
-  // Let's sign the input 
-  try {
-    decoded.signInput(1, keyPair);
-    decoded.validateSignaturesOfInput(1);
-  } catch (e) { console.error(e) }
-  // finalize all inputs
-  decoded.finalizeInput(1);
+}
 
-  const base64WithFee = decoded.toBase64();
-  const decodedWithFee = liquid.Psbt.fromBase64(base64WithFee);
-
-  decodedWithFee.signInput(0, keyPair2);
-  decodedWithFee.validateSignaturesOfInput(0);
-  decodedWithFee.finalizeInput(0);
+function sign(base64) {
+  const decodedWithFee = liquid.Psbt.fromBase64(base64)
+  decodedWithFee.signInput(0, keyPair2)
+  decodedWithFee.validateSignaturesOfInput(0)
+  decodedWithFee.finalizeInput(0)
 
   console.log(decodedWithFee.toBase64())
   console.log()
@@ -151,6 +155,16 @@ async function main() {
   console.log(hex);
 
 }
+
+
+//sign("cHNidP8BAP1PAQIAAAAAApw0tA986B4seYAv1oPzHZlegotgPCjUTJBawSG8esi1AQAAAAD/////kK7RAqG4T1AyhW8O75Jlc6bafIdvAeaqOHJ/ETN1nFUBAAAAAP////8EAdmWSKoSfJfRwDfVppO0T9DwGw+RF+Wyc17IPg7ZI/EYAQAAAAEqBfIAABYAFLWZ710/FsLh2zNm844bL2u3SpvsAdmWSKoSfJfRwDfVppO0T9DwGw+RF+Wyc17IPg7ZI/EYAQAAAAEqBfIAABYAFEYguhpN/Bt7hQOzfp6+rPZ1cir6ASWyUQcOKcoZBDzzPM1zJOLdqwPsxK4LXnfE/A5c9slaAQAAAAAF9d4tABYAFGWb7bXT08erEtf4UyPDobbAYO++ASWyUQcOKcoZBDzzPM1zJOLdqwPsxK4LXnfE/A5c9slaAQAAAAAAAALTAAAAAAAAAAEBQgHZlkiqEnyX0cA31aaTtE/Q8BsPkRflsnNeyD4O2SPxGAEAAAABKgXyAAAWABRGILoaTfwbe4UDs36evqz2dXIq+gABAUIBJbJRBw4pyhkEPPM8zXMk4t2rA+zErgted8T8Dlz2yVoBAAAAAAX14QAAFgAUZZvttdPTx6sS1/hTI8OhtsBg774BCGwCSDBFAiEA/M9skJMbw7U1mRdEahrxZWwhQHK1rP3ruAeHHoQI7bICIB5uZk0C0U1+M8qf5Yn5bio/OKCj/QrFe2qa0VB74y5eASECUUZEIPzJii5M00ev4ooy12kofazYYUdquFi6pDvTCPMAAAAAAA==")
+
+async function main() {
+  //console.log(await build());
+  sign("cHNidP8BAP1PAQIAAAAAApw0tA986B4seYAv1oPzHZlegotgPCjUTJBawSG8esi1AQAAAAD/////PaYQ9Th1iOhGJKrwf4Jhw07bhdjufsZzpeB39d+ynbAAAAAAAP////8EAdmWSKoSfJfRwDfVppO0T9DwGw+RF+Wyc17IPg7ZI/EYAQAAAAAAD0JAABYAFLWZ710/FsLh2zNm844bL2u3SpvsAdmWSKoSfJfRwDfVppO0T9DwGw+RF+Wyc17IPg7ZI/EYAQAAAAEp9q/AABYAFEYguhpN/Bt7hQOzfp6+rPZ1cir6ASWyUQcOKcoZBDzzPM1zJOLdqwPsxK4LXnfE/A5c9slaAQAAAAAF9d4tABYAFGWb7bXT08erEtf4UyPDobbAYO++ASWyUQcOKcoZBDzzPM1zJOLdqwPsxK4LXnfE/A5c9slaAQAAAAAAAALTAAAAAAAAAAEBQgHZlkiqEnyX0cA31aaTtE/Q8BsPkRflsnNeyD4O2SPxGAEAAAABKgXyAAAWABRGILoaTfwbe4UDs36evqz2dXIq+gABAUIBJbJRBw4pyhkEPPM8zXMk4t2rA+zErgted8T8Dlz2yVoBAAAAAAX14QAAFgAUZZvttdPTx6sS1/hTI8OhtsBg774BCGwCSDBFAiEAoHRhE1w60rB2kiss0AOoYcEZZzc/TLGESLewsgro6EYCIHnGx/m7WbEuj2VzwoRnVnKB/zJrjDrE1/SGKWbZXNzhASECUUZEIPzJii5M00ev4ooy12kofazYYUdquFi6pDvTCPMAAAAAAA==")
+}
+
+
 
 
 async function faucet(address) {
@@ -170,6 +184,7 @@ async function fetchUtxos(address, asset) {
 }
 
 main()
+
 
 
 
